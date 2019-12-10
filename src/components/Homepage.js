53ad13a4d085268493425ad756d491e0228d59ba
Peyton Doyle <p6doyle@gmail.com>
2019-12-10 13:56:01 -0500

    tryna get sparkles goin

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -3,10 +3,12 @@ import Becker from "./Work/Becker.js"
 import MED from "./Work/MED.js"
 import UGroup from "./Work/UGroup.js"
 
+let sparkles = "https://drive.google.com/uc?id=1JEL0BhIaqYKie3eyrv9jeb_ALus1y4A5"
 
 function App() {
   return (
     <header className="App-header">
+      <img src="sparkles"></img>
       <h1>peyton doyle's portfolio</h1>
       <p>
         coming soon.

b6e6440b15e106838c268169987bf726d7b3cfb4
Peyton Doyle <p6doyle@gmail.com>
2019-12-10 14:06:40 -0500

    added sparkles & styling

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -3,12 +3,10 @@ import Becker from "./Work/Becker.js"
 import MED from "./Work/MED.js"
 import UGroup from "./Work/UGroup.js"
 
-let sparkles = "https://drive.google.com/uc?id=1JEL0BhIaqYKie3eyrv9jeb_ALus1y4A5"
-
 function App() {
   return (
     <header className="App-header">
-      <img src="sparkles"></img>
+      <img class="sparkles" src="https://i.imgur.com/l28ys4z.png"></img>
       <h1>peyton doyle's portfolio</h1>
       <p>
         coming soon.

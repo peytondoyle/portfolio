b798ab6d9d34071d2c7be5d3c62bcbd5896fe7a6
Peyton Doyle <p6doyle@gmail.com>
2019-12-13 11:25:48 -0500

    added phone #

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -13,7 +13,7 @@ function App() {
       </span>
       <div class="columnContainer">
         <h2>Contact</h2>
-         <div class="columnL"><a href="mailto:p6doyle@gmail.com">Email</a><br></br><a href="mailto:p6doyle@gmail.com">Email</a></div>
+         <div class="columnL"><a href="mailto:p6doyle@gmail.com">Email</a><br></br><a href="tel:434-941-0738">Phone</a></div>
          <div class="columnR">asdf</div>
       </div>
     </header>

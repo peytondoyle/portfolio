c04a4cbed0a21800c6bec5e0c1f327455ad1f724
Peyton Doyle <p6doyle@gmail.com>
2019-12-13 10:19:00 -0500

    trying to fix heroku deployment?

--- src/components/Work/Becker.js
+++ src/components/Work/Becker.js
@@ -2,8 +2,14 @@ import React from 'react';
 
 function App() {
   return (
-
-  )
+    <header className="App-header">
+      <img class="sparkles" src="https://i.imgur.com/l28ys4z.png"></img>
+      <h1>Peyton doyle</h1>
+      <span class="subhead">
+        portfolio coming soon.
+      </span>
+    </header>
+  );
 }
 
 export default App;
